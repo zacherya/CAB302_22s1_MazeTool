a609e515ec759ac99696ac2f689009140919e858
Zac Adams <77133479+zacherya@users.noreply.github.com>
2022-04-30 23:38:01 +1000

    Updated maze to new MVC model

--- src/Views/MazeView.java
+++ src/Views/MazeView.java
@@ -9,31 +9,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /** The MazeView Class */
-public class MazeView extends DefaultView {
+public class MazeView extends DefaultView<MazeViewController> {
 
-    private MazeViewController controller;
+    private MazeViewController _controller;
 
     private static final int BOTTOM_BTN_WIDTH = 0;
     private static final int BOTTOM_BTN_HEIGHT = 40;
     private static final int RIGHT_BTN_WIDTH = 175;
     private static final int RIGHT_BTN_HEIGHT = 30;
 
-    // Top panel + child components
-    private JPanel topPanel;
-    private JTextArea titleText;
-    private JButton backBtn;
 
-    // Bottom panel + child components
-    private JPanel bottomPanel;
-    private JButton saveBtn;
-    private JButton exportBtn;
-
-    // Right panel + child components
-    private JPanel rightPanel;
-    private JButton solutionBtn;
-    private JButton logoImageBtn;
-    private JButton entryImageBtn;
-    private JButton exitImageBtn;
 
     // File selectors
     private JFileChooser logoImageSelector = new JFileChooser();
@@ -48,7 +33,7 @@ public class MazeView extends DefaultView {
         configureFrame();
 
         //Set Controller
-        this.controller = controller;
+        _controller = controller;
 
         readyFrame(this::addElements);
 
@@ -56,63 +41,60 @@ public class MazeView extends DefaultView {
 
     private void addElements() {
         // Create panels
-        topPanel = new JPanel();
-        bottomPanel = new JPanel();
-        rightPanel = new JPanel();
-        JPanel leftPanel = new JPanel();
-        JPanel centrePanel = new JPanel();
-        centrePanel.setBackground(Color.WHITE);
+        panels.put("primary", new JPanel());
+        panels.put("topPanel", new JPanel());
+        panels.put("bottomPanel", new JPanel());
+        panels.put("rightPanel", new JPanel());
+        panels.put("centrePanel", new JPanel());
+        panels.put("leftPanel", new JPanel());
 
-        // Create components for top panel
-        titleText = new JTextArea(1,57);
-        titleText.setText("Give your new maze a creative title..");
-        backBtn = createButton("←", 55, 30, this::backBtnAction);
+        // Define panel custom attributes
+        panels.get("centrePanel").setBackground(Color.WHITE);
+
+        // Create adn configure components for top panel
+        textAreas.put("titleText",new JTextArea(1,57));
+        textAreas.get("titleText").setText("Give your new maze a creative title..");
+
+        buttons.put("backBtn",createButton("←", 55, 30, this::backBtnAction));
 
         // Create components for bottom panel
-        saveBtn = createButton("Save", BOTTOM_BTN_WIDTH, BOTTOM_BTN_HEIGHT, this::saveBtnAction);
-        exportBtn = createButton("Export", BOTTOM_BTN_WIDTH, BOTTOM_BTN_HEIGHT,  this::exportBtnAction);
+        buttons.put("saveBtn",createButton("Save", BOTTOM_BTN_WIDTH, BOTTOM_BTN_HEIGHT, this::saveBtnAction));
+        buttons.put("exportBtn",createButton("Export", BOTTOM_BTN_WIDTH, BOTTOM_BTN_HEIGHT, this::exportBtnAction));
 
         // Create components for right panel
-        solutionBtn = createButton("Show solution", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::solutionBtnAction);
-        logoImageBtn = createButton("Select logo image", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::logoImageBtnAction);
-        entryImageBtn = createButton("Select entry image", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::entryImageBtnAction);
-        exitImageBtn = createButton("Select exit image", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::exitImageBtnAction);
+        buttons.put("solutionBtn",createButton("Show solution", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::solutionBtnAction));
+        buttons.put("logoImageBtn",createButton("Select logo image", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::logoImageBtnAction));
+        buttons.put("entryImageBtn",createButton("Select entry image", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::entryImageBtnAction));
+        buttons.put("exitImageBtn",createButton("Select exit image", RIGHT_BTN_WIDTH, RIGHT_BTN_HEIGHT, this::exitImageBtnAction));
+
+        // Define Panel grids
+        definePanelGrid("primary",1,1,0,0);
+//        definePanelGrid("topPanel",1,2,5,0);
+        definePanelGrid("bottomPanel",1,2,5,0);
+        definePanelGrid("rightPanel",6,1,0,5);
+
+        // Add elements to panels
+        addButtonsToPanel(panels.get("topPanel"),"backBtn");
+        addTextAreasToPanel(panels.get("topPanel"),"titleText");
+
+        addButtonsToPanel(panels.get("rightPanel"),"solutionBtn","logoImageBtn","entryImageBtn","exitImageBtn");
+
+        addButtonsToPanel(panels.get("bottomPanel"),"saveBtn","exportBtn");
 
         // Add panels to the JFrame
-        getContentPane().add(topPanel, BorderLayout.PAGE_START);
-        getContentPane().add(bottomPanel, BorderLayout.PAGE_END);
-        getContentPane().add(rightPanel, BorderLayout.LINE_END);
-        getContentPane().add(leftPanel, BorderLayout.LINE_START);
-        getContentPane().add(centrePanel, BorderLayout.CENTER);
-    }
-
-    /** Components for the top panel */
-    private void layoutTopPanel() {
-        topPanel.add(backBtn);
-        topPanel.add(titleText);
-    }
-
-    /** Components for the bottom panel */
-    private void layoutBottomPanel() {
-        bottomPanel.setLayout(new GridLayout(1, 2));
-        bottomPanel.add(saveBtn);
-        bottomPanel.add(exportBtn);
-    }
-
-    /** Components for the right panel */
-    private void layoutRightPanel() {
-        rightPanel.setLayout(new GridLayout(6, 1));
-        rightPanel.add(solutionBtn);
-        rightPanel.add(logoImageBtn);
-        rightPanel.add(entryImageBtn);
-        rightPanel.add(exitImageBtn);
+        getContentPane().add(panels.get("topPanel"), BorderLayout.PAGE_START);
+        getContentPane().add(panels.get("bottomPanel"), BorderLayout.PAGE_END);
+        getContentPane().add(panels.get("rightPanel"), BorderLayout.LINE_END);
+        getContentPane().add(panels.get("leftPanel"), BorderLayout.LINE_START);
+        getContentPane().add(panels.get("centrePanel"), BorderLayout.CENTER);
+        getContentPane().add(panels.get("primary"));
     }
 
     /** Event for a back button
      * @param actionEvent An ActionEvent */
     private void backBtnAction(ActionEvent actionEvent) {
         System.out.println("Back button clicked");
-        controller.disposeView();
+        _controller.disposeView();
         WelcomeViewController newView = new WelcomeViewController();
     }
 
