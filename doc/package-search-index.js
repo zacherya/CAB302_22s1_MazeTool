7703543f6d7eb10c6c1ae07d1632282a7a714d48
Aaron Nolan <N11021276@qut.edu.au>
2022-06-17 12:40:30 +1000

    Remove old JavaDoc from source code

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Controllers"},{"l":"DataAccess"},{"l":"DataAccess.DtoModels"},{"l":"DataAccess.Providers"},{"l":"Exceptions"},{"l":"Helpers"},{"l":"Modals"},{"l":"Views"}];updateSearchResults();
